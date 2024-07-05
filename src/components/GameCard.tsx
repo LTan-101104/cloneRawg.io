aad2c13a04c068b612ddba62f07094b53398151d
Tan Le <letan101104@gmail.com>
2024-07-05 22:34:56 +0700

    Display critic score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
 import { Game } from '../interface/interface'
-import { Card, Image,  CardBody } from '@chakra-ui/react';
+import { Card, Image,  CardBody, HStack } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import PlatformIconList from './PlatformIconList';
+import CriticScore from './CriticScore';
 interface Props{
     game: Game;
 
@@ -13,10 +14,13 @@ const GameCard = ({game}: Props) => {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize={'2xl'}>{game.name}</Heading>
-            {/* {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text> )} */}
-            <PlatformIconList platforms={
-              game.parent_platforms.map(obj => obj.platform)
-            }/>
+            <HStack justifyContent={'space-between'}>
+              <PlatformIconList platforms={
+                game.parent_platforms.map(obj => obj.platform)
+              }/>
+              <CriticScore score={game.metacritic}/>
+
+            </HStack>
         </CardBody>
     </Card>
   )
