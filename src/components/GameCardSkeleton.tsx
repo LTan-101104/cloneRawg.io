e286a6a0cfd65f1eba21c2d062c4e61cf0ad8b67
Tan Le <letan101104@gmail.com>
2024-07-05 23:27:40 +0700

    Refactor code, avoid duplication in the skeleton and gamecard

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card width={'400px'} borderRadius={'10px'}>
+    <Card  borderRadius={'10px'}>
         <Skeleton height={'200px'}/>
         <CardBody>
             <SkeletonText />
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,18 +4,22 @@ import useGames from '../hooks/useGames'
 import { px } from 'framer-motion';
 import GameCard from './GameCard';
 import GameCardSkeleton from './GameCardSkeleton';
-
+import GameCardContainer from './GameCardContainer';
 const GameGrid = () => {
     const {games, error, isLoading} = useGames();
-    const skeletons = Array<number>(6).fill(1).map((_v, i) => i)
+    const skeletons = Array<number>(12).fill(1).map((_v, i) => i)
     return (
         <>
         {
             error ? <Text>{error}</Text> :
                 <SimpleGrid columns={[1, 2, 3]} spacing={'10px'} margin={'10px'}>
-                    {isLoading && skeletons.map(s => <GameCardSkeleton key={s}/>)}
+                    {isLoading && skeletons.map(s => <GameCardContainer>
+                        <GameCardSkeleton key={s}/>
+                    </GameCardContainer>)}
                     {games.map(game => 
-                        <GameCard key={game.id} game={game} />
+                        <GameCardContainer>
+                            <GameCard key={game.id} game={game} />
+                        </GameCardContainer>
                     )}
                 </SimpleGrid>
         }
